6d65e421033b5f21015010034db424e6de008030
Tonussi <erictonussi@gmail.com>
2016-07-24 19:07:02 -0300

    Tela "empresas"

--- app/pages/app/empresas.php
+++ app/pages/app/empresas.php
@@ -1,1 +1,97 @@
+<button type="submit" class="btn btn-primary pull-right -upper"><small>CADASTRAR NOVA</small></button>
 <h4 class="-upper -dashed">Empresas</h4>
+
+<div class="table-responsive">
+  <table class="table table-hover">
+    <thead>
+      <th>Nome <img src="/images/icons/filter.png"</th>
+        <th><a href="#">Segmento</a></th>
+        <th><a href="#">Off-Limits Início</a></th>
+        <th><a href="#">Off-Limits Término</a></th>
+        <th><a href="#">Promessa</a></th>
+        <th><a href="#">Entrega</a></th>
+    </thead>
+    <tbody>
+      <tr>
+        <td><a href="#">Adidas</a></td>
+        <td>Life Science</td>
+        <td>-</td>
+        <td>-</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Allianz</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Amanco</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Bosch</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Boticário</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Brastemp</a></td>
+        <td>Life Science</td>
+        <td>-</td>
+        <td>-</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Camicado</a></td>
+        <td>Life Science</td>
+        <td>-</td>
+        <td>-</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Carrefour</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Danone</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+      <tr>
+        <td><a href="#">Electrolux</a></td>
+        <td>Life Science</td>
+        <td>30/01/2016</td>
+        <td>31/12/2016</td>
+        <td>30/06/2016</td>
+        <td>30/06/2016</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
